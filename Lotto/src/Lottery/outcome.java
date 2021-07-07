627e5986688ea104df0ed0b8cc79ecd4547c469a
jeongin <jeongin0520@naver.com>
2021-07-07 17:59:25 +0900

    add outcome

--- Lotto/src/Lottery/outcome.java
+++ Lotto/src/Lottery/outcome.java
@@ -1,4 +1,5 @@
 package Lottery;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -7,31 +8,32 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 /*
 4) 당첨번호, 총 구매 수, 재도전, 종료 버튼 
 *  숫자 90개(1~45) : 당첨됐을 때 숫자 색 / 당첨 안됐을 때 숫자 색 
 *  당첨, 낙첨, 1등, 2등, 3등, 4등, 5등 글씨 
-*/ 
+*/
 class outcome extends JFrame {
 	public outcome(int[][] selectedNum) {
 		JPanel result = new JPanel();
 		result.setLayout(new BoxLayout(result, BoxLayout.Y_AXIS));
-		
+
 //		String a = "";
 		List<Integer> outcomeNum = MakeNumber.randomNum();
-		
-		int bonusNum = outcomeNum.get(6);	//	보너스넘버 
+
+		int bonusNum = outcomeNum.get(6); // 보너스넘버
 		outcomeNum.remove(6);
 		// 1 2 3 4 5 6 + 8
 		Collections.sort(outcomeNum);
-		
+
 		JPanel winNumPnl = new JPanel();
 		ImageIcon winNumIcon = new ImageIcon("Score/winningNum.gif");
 		JLabel winNum = new JLabel(winNumIcon);
 		winNumPnl.add(winNum);
-		
+
 		JPanel imDone = new JPanel();
-		
+
 		for (int i = 0; i < 6; i++) {
 			int nowNum = outcomeNum.get(i);
 			ImageIcon winNumsIcon = new ImageIcon("number/" + nowNum + ".gif");
@@ -39,10 +41,21 @@ class outcome extends JFrame {
 			winNumPnl.add(winNums);
 		}
 		
+		ImageIcon bonus = new ImageIcon("number/add.gif");
+		ImageIcon bonus2 = new ImageIcon("number/" + bonusNum + ".gif");
+		JLabel lbl;
+		JLabel lbl2;
+
+		lbl = new JLabel(bonus);
+		lbl2 = new JLabel(bonus2);
+
+		winNumPnl.add(lbl);
+		winNumPnl.add(lbl2);
+
 		for (int i = 0; i < selectedNum.length; i++) {
 			int count = 0;
-			boolean bonusChk = false;	//	보너스 번호를 가졌는지 아닌지 체크 
-			
+			boolean bonusChk = false; // 보너스 번호를 가졌는지 아닌지 체크
+
 			for (int j = 0; j < 6; j++) {
 				int tmpNum = selectedNum[i][j];
 				boolean checkNum = outcomeNum.contains(tmpNum);
@@ -52,66 +65,56 @@ class outcome extends JFrame {
 					ImageIcon img = new ImageIcon("number/" + tmpNum + ".gif");
 					winNum.add(ociamge);
 					count++;
-				}
-				else {
+				} else {
 					// 라벨 추가 미당첨이니까 흑백 라벨 imageicon("number\\" + 숫자 + "b.gif")
 					JLabel ociamge2 = new JLabel();
 					ImageIcon img = new ImageIcon("number/" + tmpNum + "b.gif");
 					winNum.add(ociamge2);
 				}
 				if (tmpNum == bonusNum)
-					bonusChk = true;			// 보너스 넘버를 가지고 있는걸 체크함 
+					bonusChk = true; // 보너스 넘버를 가지고 있는걸 체크함
 			}
 			// 한묶음의 선택한 숫자 당첨 확인이 끝나고 나서
 			// count가 5라면 이 배열이 뽀나쓰 넘버를 들고있는지 확인해야함
 			// 왜냐하면 2등은 숫자 5개와 뽀나쓰 넘버가 맞을때 이기 때문
 			if (count == 5 && bonusChk) { // 이건 2등
-			
+
 			} else {
-			JLabel realResult = null;
-			switch(count) {
-			case 6: 
-				ImageIcon tmp = new ImageIcon("Score/1st.gif"); 
-				realResult = new JLabel(tmp);
-				break;
-			case 5: 
-				ImageIcon tmp2 = new ImageIcon("Score/1st.gif");
-				realResult = new JLabel(tmp2);
-				break;
-			case 4:
-				ImageIcon tmp3 = new ImageIcon("Score/4th.gif");
-				realResult = new JLabel(tmp3);
-				break;
-			case 3:
-				ImageIcon tmp4 = new ImageIcon("Score/5th.gif"); 
-				realResult = new JLabel(tmp4);
-				break;
-				default: 
-					ImageIcon tmp5 = new ImageIcon("Score/unlucky.gif"); 
+				JLabel realResult = null;
+				switch (count) {
+				case 6:
+					ImageIcon tmp = new ImageIcon("Score/1st.gif");
+					realResult = new JLabel(tmp);
+					break;
+				case 5:
+					ImageIcon tmp2 = new ImageIcon("Score/1st.gif");
+					realResult = new JLabel(tmp2);
+					break;
+				case 4:
+					ImageIcon tmp3 = new ImageIcon("Score/4th.gif");
+					realResult = new JLabel(tmp3);
+					break;
+				case 3:
+					ImageIcon tmp4 = new ImageIcon("Score/5th.gif");
+					realResult = new JLabel(tmp4);
+					break;
+				default:
+					ImageIcon tmp5 = new ImageIcon("Score/unlucky.gif");
 					realResult = new JLabel(tmp5);
 					break;
-			}
+				}
 				imDone.add(realResult);
 			}
 		}
-		ImageIcon bonus = new ImageIcon("number/add.gif" );
-		ImageIcon bonus2 = new ImageIcon("number/" + bonusNum + ".gif");
-		JLabel lbl;
-		JLabel lbl2;
-		
-		lbl = new JLabel(bonus);
-		lbl2 = new JLabel(bonus2);
-		
-		winNumPnl.add(lbl);
-		winNumPnl.add(lbl2);
-		
+
 		result.add(winNumPnl);
 		result.add(imDone);
-		
+
 		add(result);
-		
+
 		showGUI();
 	}
+
 	private void showGUI() {
 //		setSize(500, 500);
 		pack();
@@ -119,6 +122,3 @@ class outcome extends JFrame {
 		setVisible(true);
 	}
 }
-
-
-
